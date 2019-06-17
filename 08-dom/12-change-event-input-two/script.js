0b1d11e42583de00a63892443ba2713dc8f28dbc
e160068 <p.haerens@student.helmo.be>
2019-06-17 09:19:09 +0200

    blbl

--- 08-dom/12-change-event-input-two/script.js
+++ 08-dom/12-change-event-input-two/script.js
@@ -16,11 +16,11 @@
         let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
-    
+
     function valideInput(chaine){
-        let regex1 = /^[a-zA-Z0-9]{8,}$/;
+        let regex1 = /[.]{8,}/;
         
-        let regex2 = /^.*[0-9].*[0-9].*$/;
+        let regex2 = /.*[0-9].*[0-9].*/;
 
         return regex1.test(chaine) && regex2.test(chaine);
 
